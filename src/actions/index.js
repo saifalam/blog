53c87c739e78ac5de0f580fbbb7c6def05228368
alam <alam@iabg.de>
2021-04-14 14:44:02 +0200

    _. memoize is used to reduce network call and _.chain is used as experiment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,46 @@
 import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonPlaceholder';
 
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    //-------- Chain version to get unique userId with less netwoek call  --------
+    /* _.chain(getState().posts)
+         .map('userId')
+         .uniq()
+         .forEach(id => dispatch(fetchUser(id)))
+         .value();*/
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonplaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
+// -------- Normal version of fetchUser --------
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+//-------  _.memoize version of fetchUser  --------
+/*
 export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
 
 // _ indicates that, it is a private function
 const _fetchUser = _.memoize(async(id, dispatch) => {
     const response = await jsonplaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+});*/
 
 
-/*export const fetchUser = function (id) { 
+/*export const fetchUser = function (id) {
     return _.memoize(async function (dispatch)  {
         const response = await jsonplaceholder.get(`/users/${id}`);
         dispatch({ type: 'FETCH_USER', payload: response.data });
     });
 };*/
+
